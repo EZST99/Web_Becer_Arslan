40d7b7554f096ffab7f9e35ef1eed4a2928f40c4
Irem Arslan <145445376+EZST99@users.noreply.github.com>
2023-11-08 19:22:02 +0100

    Anpassungen Registrierung & Login

--- html/login.php
+++ html/login.php
@@ -4,12 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <title>Login</title>
     <style>
-        *{
+        * {
             padding: 0;
             margin: 0;
             box-sizing: border-box;
@@ -56,9 +55,28 @@
 <body>
     <?php
     include 'nav.php';
+
+    if (($_SERVER["REQUEST_METHOD"] == "POST") && isset($_POST["username"]) && isset($_POST["password"])) {
+        if ($_POST["username"] == "user" && $_POST["password"] == "123") {
+            setcookie("loginCookie", $_POST["username"], time()+3600);
+            header("Location: index.php");
+            exit;
+        }
+    }
+
+   /* session_start();
+        if (isset($_POST["username"]) && isset($_POST["password"])) {
+            if ($_POST["username"] == $_POST["password"]) {
+                // Login erfolgreich
+                $_SESSION["usernameSession"] = $_POST["username"];
+            } else {
+                // Login nicht erfolgreich
+                echo "Schleich dich!";
+            }
+        }*/
     ?>
     <section class="Form my-4 mx-5">
-        <form action="/action_page.php" class="was-validated">
+        <form action="../html/login.php" class="was-validated" method="POST">
             <div class="container">
                 <div class="row no-gutters">
                     <div class="col-lg-5 p-0">
@@ -70,30 +88,31 @@
                         <form>
                             <div class="form-row">
                                 <div class="col-lg-7">
-                                    <input type="username" placeholder="Username" class="form-control my-3 p-4"
-                                        required>
+                                    <input name ="username" type="username" placeholder="Username" class="form-control my-3 p-4" required>
                                     <div class="valid-feedback"></div>
                                     <div class="invalid-feedback">Bitte ausfüllen.</div>
                                 </div>
                             </div>
                             <div class="form-row">
                                 <div class="col-lg-7">
-                                    <input type="password" placeholder="******" class="form-control my-3 p-4" required>
+                                    <input name ="password" type="password" placeholder="******" class="form-control my-3 p-4" required>
                                     <div class="valid-feedback"></div>
                                     <div class="invalid-feedback">Bitte ausfüllen.</div>
                                 </div>
                             </div>
                             <div class="form-row">
                                 <div class="col-lg-7">
-                                    <button type="button" class="btn1 mt-3 mb-5">Login</button>
+                                    <button type="submit" class="btn1 mt-3 mb-5">Login</button>
                                 </div>
                             </div>
                             <a href="#">Passwort vergessen?</a>
-                            <p>Noch gar keinen Account? <a href="registrierung.html">Registriere dich hier</a></p>
+                            <p>Noch gar keinen Account? <a href="registrierung.php">Registriere dich hier</a></p>
                         </form>
                     </div>
                 </div>
+        
             </div>
     </section>
 
 </html>
+
--- html/registrierung.php
+++ html/registrierung.php
@@ -4,43 +4,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <title>Registrierung</title>
     <style>
+        * {
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+        }
+
         body {
             background: rgb(214, 198, 180);
         }
 
-        .form-container {
-            border: 2px solid rgba(108, 73, 9, 0.671);
-            border-radius: 10px;
-            padding: 10px;
-            /* Verkleinert den Innenabstand der Box */
-            margin-top: 20px;
+        .row {
             background: white;
-            box-shadow: 12px 12px 22px rgb(33, 32, 32);
-            max-width: 800px;
-            /* Erhöht die maximale Breite des Containers */
-            margin: 0 auto;
-            /* Zentriert den Container horizontal */
         }
 
         .img-fluid {
             width: 100%;
-            /* Das Bild nimmt 100% der Breite des Elternelements ein */
-            height: auto;
-            /* Die Höhe des Bildes passt sich automatisch an */
-            border-top-left-radius: 10px;
-            border-top-right-radius: 10px;
-            margin: 0;
-            padding: ;
-        }
-
-        .form-content {
-            padding: 20px;
-            /* Innenabstand für den Inhalt innerhalb der Box */
+            height: 100%;
+            object-fit: cover;
+            border-top-right-radius: 30px;
+            border-bottom-right-radius: 30px;
         }
 
         .btn1 {
@@ -59,110 +46,104 @@
             border: 1px solid;
             color: rgba(108, 73, 9, 0.671);
         }
-    </style>
-</head>
 
+        .error-message {
+            color: red;
+        }
+    </style>
+
+</head>
 
 <body>
     <?php
     include 'nav.php';
     ?>
-    <section class="Form my-4 mx-auto">
-        <form action="/action_page.php" class="was-validated">
-            <div class="container-fluid text-center">
-                <div class="row no-gutters">
-                    <div class="col-lg-12">
-                        <div class="form-container"> <!-- Hier wird die Box um das gesamte Formular hinzugefügt -->
-                            <img src="../images/Old_Lesbians.jpg" class="img-fluid" alt="Gäste Bild">
-                            <!-- Das Bild wird ganz oben innerhalb der Box platziert -->
-                            <div class="p-3"> <!-- Der restliche Inhalt, der unterhalb des Bildes erscheinen soll -->
-                                <h1 class="font-weight-bold py-3">Hotel Helios</h1>
-                                <h4>Account registrieren</h4>
-                                <form>
-                                    <div class="row">
-                                        <div class="col-md-6">
-                                            <div class="form-row">
-                                                <input id="anrede" type="text" class="form-control my-3 p-4"
-                                                    placeholder="Anrede" required>
-                                                <div class="valid-feedback"></div>
-                                                <div class="invalid-feedback"></div>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-6">
-                                            <div class="form-row">
-                                                <input id="vorname" type="text" class="form-control my-3 p-4"
-                                                    placeholder="Vorname" required>
-                                                <div class="valid-feedback"></div>
-                                                <div class="invalid-feedback"></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-6">
-                                            <div class="form-row">
-                                                <input id="nachname" type="text" class="form-control my-3 p-4"
-                                                    placeholder="Nachname" required>
-                                                <div class="valid-feedback"></div>
-                                                <div class="invalid-feedback"></div>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-6">
-                                            <div class="form-row">
-                                                <input id="email" type="text" class="form-control my-3 p-4"
-                                                    placeholder="E-Mail" required>
-                                                <div class="valid-feedback"></div>
-                                                <div class="invalid-feedback"></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-6">
-                                            <div class="form-row">
-                                                <input id="geburtsdatum" type="date" class="form-control my-3 p-4"
-                                                    required>
-                                                <div class="valid-feedback"></div>
-                                                <div class="invalid-feedback"></div>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-6">
-                                            <div class="form-row">
-                                                <input id="username" type="text" class="form-control my-3 p-4"
-                                                    placeholder="Username" required>
-                                                <div class="valid-feedback"></div>
-                                                <div class="invalid-feedback"></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-6">
-                                            <div class="form-row">
-                                                <input id="password" type="password" class="form-control my-3 p-4"
-                                                    placeholder="Passwort" required>
-                                                <div class="valid-feedback"></div>
-                                                <div class="invalid-feedback"></div>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-6">
-                                            <div class="form-row">
-                                                <input id="repassword" type="password" class="form-control my-3 p-4"
-                                                    placeholder="Passwort wiederholen" required>
-                                                <div class="valid-feedback"></div>
-                                                <div class="invalid-feedback"></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="form-row">
-                                        <button type="button" class="btn1 mt-3 mb-5">Registrieren</button>
-                                    </div>
-                                </form>
-                                <a href="#">Passwort vergessen?</a>
-                                <p>Noch gar keinen Account? <a href="registrierung.html">Registriere dich hier</a></p>
+
+    <section class="Form my-4 mx-5">
+        <div class="container">
+            <div class="row no-gutters" style="border-radius: 30px; box-shadow: 12px 12px 22px rgb(33, 32, 32);">
+                <div class="col-lg-7 px-5 pt-5">
+                    <h1 class="font-weight-bold py-3">Hotel Helios</h1>
+                    <h4>Registrieren</h4>
+                    <form method="POST" action="../html/action_page.php" class="was-validated">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-row">
+                                    <input name="anrede" type="text" class="form-control my-3 p-4" placeholder="Anrede" required>
+                                    <div class="valid-feedback"></div>
+                                    <div class="invalid-feedback"></div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-row">
+                                    <input name="vorname" type="text" class="form-control my-3 p-4" placeholder="Vorname" required>
+                                    <div class="valid-feedback"></div>
+                                    <div class="invalid-feedback"></div>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-row">
+                                    <input name="nachname" type="text" class="form-control my-3 p-4" placeholder="Nachname" required>
+                                    <div class="valid-feedback"></div>
+                                    <div class="invalid-feedback"></div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-row">
+                                    <input name="email" type="email" class="form-control my-3 p-4" placeholder="E-Mail" required>
+                                    <div class="valid-feedback"></div>
+                                    <div class="invalid-feedback"></div>
+                                </div>
+                                <?php if (!empty($error_message) && empty($_POST["email"])) {
+                                    echo "<span class='error-message'>$error_message</span>";
+                                } ?>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-row">
+                                    <input name="geburtsdatum" type="date" class="form-control my-3 p-4" required>
+                                    <div class="valid-feedback"></div>
+                                    <div class="invalid-feedback"></div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-row">
+                                    <input name="username" type="text" class="form-control my-3 p-4" placeholder="Username" required>
+                                    <div class="valid-feedback"></div>
+                                    <div class="invalid-feedback"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-row">
+                                    <input name="password" type="password" class="form-control my-3 p-4" placeholder="Passwort" required>
+                                    <div class="valid-feedback"></div>
+                                    <div class="invalid-feedback"></div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-row">
+                                    <input name="repassword" type="password" class="form-control my-3 p-4" placeholder="Passwort wiederholen" required>
+                                    <div class="valid-feedback"></div>
+                                    <div class="invalid-feedback"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <button type="submit" class="btn1 mt-3 mb-5">Registrieren</button>
+                        </div>
+                    </form>
+                    <p>Bereits ein Account? <a href="login.php">Melde dich an</a></p>
+                </div>
+                <div class="col-lg-5 p-0">
+                    <img src="../images/Old_Lesbians_New.png" class="img-fluid p-0 m-0" alt="Gäste Bild">
                 </div>
             </div>
-        </form>
+        </div>
     </section>
 </body>
 
