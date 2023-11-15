303cced5cbb5687f819c6c57f1642c9d36ee755c
Irem Arslan <145445376+EZST99@users.noreply.github.com>
2023-11-15 21:14:41 +0100

    Kommentar

--- html/login.php
+++ html/login.php
@@ -1,3 +1,28 @@
+<?php
+include 'nav.php';
+
+session_start();
+
+/*if (isset($_GET["logout"]) && $_GET["logout"] == "true") {
+
+    unset($_SESSION["newsession"]);
+
+    header("Location: ../html/");
+
+
+    session_destroy();
+    die("You signed out...");
+}*/
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && $_POST["username"] == "admin" && $_POST["password"] == "admin") {
+
+    $_SESSION["user"] = $_POST["username"];
+    setcookie("loginCookie", $_POST["username"], time() + 3600);
+    header("Location: index.php");
+}
+?>
+
+
 <!DOCTYPE html>
 <html>
 
@@ -53,49 +78,27 @@
 </head>
 
 <body>
-    <?php
-    include 'nav.php';
-
-    if (($_SERVER["REQUEST_METHOD"] == "POST") && isset($_POST["username"]) && isset($_POST["password"])) {
-        if ($_POST["username"] == "user" && $_POST["password"] == "123") {
-            setcookie("loginCookie", $_POST["username"], time()+3600);
-            header("Location: index.php");
-            exit;
-        }
-    }
-
-   /* session_start();
-        if (isset($_POST["username"]) && isset($_POST["password"])) {
-            if ($_POST["username"] == $_POST["password"]) {
-                // Login erfolgreich
-                $_SESSION["usernameSession"] = $_POST["username"];
-            } else {
-                // Login nicht erfolgreich
-                echo "Schleich dich!";
-            }
-        }*/
-    ?>
     <section class="Form my-4 mx-5">
-        <form action="../html/login.php" class="was-validated" method="POST">
-            <div class="container">
-                <div class="row no-gutters">
-                    <div class="col-lg-5 p-0">
-                        <img src="../images/Hotel_Login.png" class="img-fluid p-0 m-0" alt="Gäste Bild">
-                    </div>
-                    <div class="col-lg-7 px-5 pt-5">
-                        <h1 class="font-weight-bold py-3">Hotel Helios</h1>
-                        <h4>Account anmelden</h4>
-                        <form>
+        <div class="container">
+            <div class="row no-gutters">
+                <div class="col-lg-5 p-0">
+                    <img src="../images/Hotel_Login.png" class="img-fluid p-0 m-0" alt="Gäste Bild">
+                </div>
+                <div class="col-lg-7 px-5 pt-5">
+                    <h1 class="font-weight-bold py-3">Hotel Helios</h1>
+                    <h4>Account anmelden</h4>
+                    <?php if (!isset($_SESSION["user"])) : ?>
+                        <form method="POST">
                             <div class="form-row">
                                 <div class="col-lg-7">
-                                    <input name ="username" type="username" placeholder="Username" class="form-control my-3 p-4" required>
+                                    <input name="username" type="username" placeholder="Username" class="form-control my-3 p-4" required>
                                     <div class="valid-feedback"></div>
                                     <div class="invalid-feedback">Bitte ausfüllen.</div>
                                 </div>
                             </div>
                             <div class="form-row">
                                 <div class="col-lg-7">
-                                    <input name ="password" type="password" placeholder="******" class="form-control my-3 p-4" required>
+                                    <input name="password" type="password" placeholder="******" class="form-control my-3 p-4" required>
                                     <div class="valid-feedback"></div>
                                     <div class="invalid-feedback">Bitte ausfüllen.</div>
                                 </div>
@@ -108,11 +111,12 @@
                             <a href="#">Passwort vergessen?</a>
                             <p>Noch gar keinen Account? <a href="registrierung.php">Registriere dich hier</a></p>
                         </form>
-                    </div>
+                    <?php else : header("Location: index.php");?>
+                        
+                    <?php endif ?>
                 </div>
-        
             </div>
+        </div>
     </section>
 
 </html>
-
